5c7891a048ced496e743ef2e41e3d0ced9ff2677
Admin <admin@Admins-Mac.local>
2017-11-23 21:40:24 +0300

    side menu

--- Bulats_practice/ViewController.swift
+++ Bulats_practice/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Bulats_practice
 //
-//  Created by Admin on 22/11/2017.
+//  Created by Admin on 23/11/2017.
 //  Copyright © 2017 utt.fr. All rights reserved.
 //
 
@@ -10,16 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menu: UIBarButtonItem!
+    @IBOutlet weak var alert: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        sideMenu()
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func sideMenu()
+    {
+        if revealViewController() != nil {
+            menu.target = revealViewController()
+            
+            menu.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rearViewRevealWidth = 275
+            revealViewController().rightViewRevealWidth = 160
+            
+            alert.target = revealViewController()
+            alert.action = #selector(SWRevealViewController.rightRevealToggle(_:))
 
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
